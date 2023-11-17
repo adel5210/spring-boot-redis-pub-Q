33ca151deace9131c41a281a30e0b9d1f2cf8a35
adel_5210 <adel_5210m@yahoo.com>
2023-11-17 18:13:51 +0800

    1st commit

---
+++ src/test/java/com/adel/mq/redisqueue/RedisqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adel.mq.redisqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
