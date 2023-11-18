b7842bcc8a4aa9df358954025154cf25ea80338b
adel_5210 <adel_5210m@yahoo.com>
2023-11-18 14:58:20 +0800

    Enchance peresitent removal on pub

--- src/main/java/com/adel/mq/redisqueue/service/PublisherService.java
+++ src/main/java/com/adel/mq/redisqueue/service/PublisherService.java
@@ -1,6 +1,11 @@
 package com.adel.mq.redisqueue.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
@@ -19,18 +24,29 @@ public class PublisherService implements CommandLineRunner {
 
     private final RedisTemplate<String, String> credisTemplate;
     private final CeventRepository ceventRepository;
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Initiate publisher ...");
+        final List<CompletableFuture<Cevent>> processCevents = new ArrayList<>();
         for (long i = 1; i < 1000; i++) {
             final Cevent cevent = new Cevent(String.valueOf(i), UUID.randomUUID().toString());
-            ceventRepository.save(cevent);
+
+            processCevents.add(CompletableFuture.supplyAsync(() -> ceventRepository.save(cevent), executorService));
+
             final ObjectMapper mapper = new ObjectMapper();
             final String jsonString = mapper.writeValueAsString(cevent);
             this.credisTemplate.convertAndSend("topic0", jsonString);
             System.out.println(jsonString);
         }
+
+        Executors.newCachedThreadPool().execute(() -> {
+            processCevents.forEach(e -> 
+                e.thenAccept(e2 -> ceventRepository.delete(e2))
+            );
+        });
+
     }
 
 }
